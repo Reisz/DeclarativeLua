89eefb1cc0474589dc9191deb7082c4bf9bf8cee
Daniel <Reisz@users.noreply.github.com>
2017-01-31 14:36:16 +0100

    Add more tests for signals.

--- spec/signal_spec.lua
+++ spec/signal_spec.lua
@@ -1,12 +1,38 @@
 local Component = require "Component"
 
 describe("Component signals", function()
+  it("should provide basic connect functionality", function()
+    local c = Component { Component.signal("test") }()
+    local s = spy.new(function() end)
+    c:connect("test", s)
+    c:emit("test")
+    assert.spy(s).was_called(1)
+  end)
+      
+  it("should be able to pass parameters in emit", function()
+    local c = Component { Component.signal("test") }()
+    local s = spy.new(function() end)
+    c:connect("test", s)
+    c:emit("test", "123")
+    assert.spy(s).was_called(1)
+    assert.spy(s).was_called_with("123")
+  end)
+      
+  it("should be able to connect to member functions", function()
+    local c = Component { Component.signal("test") }()
+    local s = { s = spy.new(function() end) }
+    c:connect("test", s, "s")
+    c:emit("test", "123")
+    assert.spy(s.s).was_called(1)
+    assert.spy(s.s).was_called_with(s, "123")
+  end)
+    
   it("should automatically add signals for properties", function()
     local c = Component{ x = Component.property(1) }()
     local s = spy.new(function() end)
     c:connect("xChanged", s)
     c.x = 2
-    assert.spy(s).was_called()
+    assert.spy(s).was_called(1)
   end)
 
   it("should let listeners be garbage collected", function()
