fa7dcbcfe1911547f829b190c6aac39c1f523b3f
Daniel <leinad.ffafp@gmail.com>
2017-02-02 22:09:06 +0100

    Add basic matcher rules.

--- src/Component/Matcher.lua
+++ src/Component/Matcher.lua
@@ -8,7 +8,7 @@ for v in string.gmatch([[
   rawequal rawget rawlen
   tonumber tostring type
 ]], "%S+") do
-  safeMethods[v] = _G[v]
+  safeMethods["_" .. v] = _G[v]
 end
 
 local rules = setmetatable({}, { __index = safeMethods })
@@ -26,21 +26,47 @@ end
 
 -- Matchers for prototypable values need to recognize and match prototypes
 function Matcher.addRule(name, fn, ruleType)
-  ruleType = ruleType or "call"
-  assert(not rules[name]) -- TODO message
+  ruleType = ruleType or "list"
+  assert(not rules[name],
+    string.format("A rule with the name %s already exists.", name))
 
   if ruleType == "basic" then
     rules[name] = fn
-  elseif ruleType == "call" then
-    rules[name] = fn -- TODO
-  elseif ruleType == "list" then
-    rules[name] = fn -- TODO
+    return
+  end
+
+  local mt = { __call = fn }
+
+  if ruleType == "list" then
+    rules[name] = function(tbl)
+      return setmetatable(tbl, mt)
+    end
   elseif ruleType == "set" then
-    rules[name] = fn -- TODO
+    rules[name] = function(tbl)
+      local result = {}
+      for i = 1, #tbl do result[tbl[i]] = true end
+      return setmetatable(result, mt)
+    end
   end
 end
 
--- TODO add standard rules
+rules._  = function() return true  end
+rules.__ = function() return false end
+Matcher.addRule("all", function(self, v)
+  for i = 1, #self do if not self[i](v) then return false end end
+  return true
+end)
+Matcher.addRule("any", function(self, v)
+  for i = 1, #self do if self[i](v) then return true end end
+  return false
+end)
+Matcher.addRule("may", function(self, v)
+  return type(v) == "nil" or self[1](v)
+end)
+
+for _,t in ipairs {
+  "number", "string", "boolean", "table", "function", "thread", "userdata"
+} do Matcher.addRule(t, function(v) return type(v) == t end, "basic") end
 
 local matcherToken = {}
 function Matcher.new(m)
